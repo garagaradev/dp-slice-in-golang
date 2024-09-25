f2102864a37212e91ef81f12b5a2f75c73aca633
garagaradev <garagaradev@proton.me>
2024-09-25 19:38:56 +0700

    added an alternative solution

--- 11-deleting-the-first-element.go
+++ 11-deleting-the-first-element.go
@@ -7,5 +7,7 @@ func main(){
   slice := []int{10,20,30,40,50}
   fmt.Println("Initial slice:", slice)
   slice = append(slice[1:])
+  // alternative solution
+  // slice = slice[1:]
   fmt.Println("Current slice:", slice)
 }
