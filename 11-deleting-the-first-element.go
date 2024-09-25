9219f44245cb82d79584e6b8cbb56ab524d7a132
garagaradev <garagaradev@proton.me>
2024-09-25 19:37:17 +0700

    deleting the first element

---
+++ 11-deleting-the-first-element.go
@@ -0,0 +1,11 @@
+// create a slice of integers [10, 20, 30, 40, 50]
+// then delete the first element (value 10)
+package main
+import "fmt"
+
+func main(){
+  slice := []int{10,20,30,40,50}
+  fmt.Println("Initial slice:", slice)
+  slice = append(slice[1:])
+  fmt.Println("Current slice:", slice)
+}
