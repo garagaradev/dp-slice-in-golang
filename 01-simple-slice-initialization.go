809aa6bcad599c377fd66d35100b07fe1d21af01
garagaradev <garagaradev@proton.me>
2024-09-25 16:35:20 +0700

    a simple slice initialization

---
+++ 01-simple-slice-initialization.go
@@ -0,0 +1,10 @@
+// Create a slice of 5 integers with the values [10,20,30,40,50]
+// then print the elements of the slice.
+
+package main
+import "fmt"
+
+func main(){
+  slice := []int{10,20,30,40,50}
+  fmt.Println(slice)
+}
