b18cd2b13a0ae3029437bb6674ed28eb12f47319
garagaradev <garagaradev@proton.me>
2024-09-25 18:52:01 +0700

    2d slice

---
+++ 09-multi-dimensional-slice.go
@@ -0,0 +1,12 @@
+// create 2D slice containing 3 slices, each with 3 integers
+package main
+import "fmt"
+
+func main(){
+  matrix := [][]int{
+    {1,2,3},
+    {4,5,6},
+    {7,8,9},
+  }
+  fmt.Println(matrix)
+}
